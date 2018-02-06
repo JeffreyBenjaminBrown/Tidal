a62b7c523ef8b4399020169410b61c754f18ed80
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-02-05 21:50:14 -0500

    add music

--- music/loping-triads/c.hs
+++ music/loping-triads/c.hs
@@ -1,3 +1,5 @@
+-- shortly into d1, drums need to change
+
 :. d0
 v9 $ pow3 +| wubs
 
@@ -6,7 +8,7 @@ v0 $ bass $ early 0 arpeg &+ pe "d0 | *//3 d4 _ _"
 
 v1 $ high $ meta lastEarly $ chVF deg_p (+2) <$> pedaledRises
 
-v9 shh
+v9 shh -- only two bars (half a chord cycle)
 
 v9 $ meta (rev fastRevShift) (rev pow3)
 v2 $ high $ meta (pt "/4 id ea1%3 rev,fa2") pedaledRises
@@ -18,6 +20,7 @@ mapM_ ($ shh) [v0,v1,v2,v9]
 v9 $ pow3 +| wubs
 
 v0 $ bass $ early 0 arpeg &+ pe "d0 | *//3 d4 _ _" -- start before _can
+-- todo/compose : add something ambient here, starts with bass
 
 v1 $ high $ meta lastEarly $ chVF deg_p (+2) <$> pedaledRises -- hurry
 
@@ -40,4 +43,4 @@ v2 $ high $ meta (pt "/4 id ea1%3 rev,fa2") pedaledRises
 
 v8 $ meta fastRevShift wubs
 
-mapM_ ($ shh) [v8,v9,v2]
+mapM_ ($ shh) [v8,v9]
