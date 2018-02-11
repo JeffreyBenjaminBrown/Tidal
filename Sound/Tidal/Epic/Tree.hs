c884533988e195f995855be4ee82f0324bde9aec
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-02-10 21:05:32 -0500

    illustrate why trees look like a bad idea

--- Sound/Tidal/Epic/Tree.hs
+++ Sound/Tidal/Epic/Tree.hs
@@ -2,6 +2,7 @@ module Sound.Tidal.Epic.Tree where
 import Data.Tree (Tree(..), unfoldTree)
 import Sound.Tidal.Epic.Types.Reimports
 
+
 data DurNode a = DurNode {
   trepicPre :: Dur -- ^ sum of earlier durations
   , trepicDur :: Dur -- ^ duration of this one
@@ -9,6 +10,14 @@ data DurNode a = DurNode {
   , trepicLoad :: a } deriving (Show, Eq, Ord)
 type DurTree a = Tree (DurNode a)
 type Trepic a = Tree (DurNode (Maybe a))
+
+-- uh-oh this looks like a big can of worms
+-- dtSubTree :: DurTree a -> Arc -> DurTree a
+-- dtSubTree = flip $ const id
+-- After dtSubTree, do this:
+-- -- | TODO. Could base on Transform.{loope, *space*}
+-- -- could use Data.Fixed.{div', mod'}, Util.roundDownTo, lcm
+-- dtArc :: DurTree a -> Arc -> [(Arc,a)]
 
 dtPeriod :: DurTree a -> Dur
 dtPeriod dt = trepicPre n + trepicDur n + trepicPost n
