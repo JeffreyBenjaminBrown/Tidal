74ac1a99df7adf77a616f360686b81fa99ee2e88
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-02-15 22:28:20 -0500

    demostrate sorting an immutable vector as if mutable

--- Sound/Tidal/Vector/mutable.hs
+++ Sound/Tidal/Vector/mutable.hs
@@ -1,12 +1,14 @@
 import Control.Monad.ST
 import Data.Vector
-import Data.Vector.Algorithms.Intro (sort)
+import Data.Vector.Algorithms.Intro (sortBy)
 import Data.Vector.Mutable
+import Data.Ord (Ordering(..))
 
-x = fromList [1,4,2] :: Vector Int
+x = fromList [(1,101),(4,99),(2,100)] :: Vector (Int,Int)
+compare' e f = compare (fst e) (fst f)
 
-verboseCopy :: Vector Int
-verboseCopy = runST $ do
+xSorted :: Vector (Int,Int)
+xSorted = runST $ do
   v <- thaw x
-  sort v
+  sortBy compare' v
   freeze v
